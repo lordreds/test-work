f03ac3150506ee946a78cb9b41baf86a5f3dee40
lordreds <35828219+lordreds@users.noreply.github.com>
2020-10-31 03:23:36 +0500

    Update js.js

--- js.js
+++ js.js
@@ -8,14 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
             a.children[index].innerHTML,
             b.children[index].innerHTML
         );
-        
+
         for(const tBody of target.closest('table').tBodies)
             tBody.append(...[...tBody.rows].sort(comparator(index, order)));
 
         for(const cell of target.parentNode.cells)
             cell.classList.toggle('sorted', cell === target);
+
+        document.querySelectorAll('.table_sort tbody tr td:first-child').forEach((el, i) => el.textContent = i + 1);
     };
-    
+
     document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
-    
+
 });
